a64f8eaa9bee35d66a33e59389d9003f471ba5ed
casperdaisy <casperdaisy2009@gmail.com>
2026-09-10 19:44:22 +0500

    Add files via upload

---
+++ lesson 3/my weather report.py
@@ -0,0 +1,33 @@
+# PART 1 - USER INPUT
+city = input("Enter your city name:")
+temp = float(input("Enter today's temperature in C:"))
+
+# PART 2 - IF STATEMENT
+if temp > 35:
+    print("Warning: It is very hot today!")
+
+# PART 3 - IF-ELSE
+if temp > 25:
+    print("Great day to go outside!")
+else:
+    print("Grab a jacket before you go out!")
+
+# PART 4 - IF-ELIF-ELSE
+if temp > 35:
+    print("Weather: Scorching Hot")
+elif temp > 25:
+    print("Weather: Warm and Sunny")
+elif temp > 15:
+    print("Weather: Cool and Breezy")
+else:
+    print("Weather: Cold - stay warm!")
+
+# PART 5 - DATETIME MODULE
+import datetime
+import calendar
+
+now = datetime.datetime.now()
+print("City:", city)
+print("Time now:", now)
+
+print(calendar.calendar(now.year))
